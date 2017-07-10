2450fd0914e8faf3774d7e380c7611e5372038a0
Administrator <2364522919@qq.com>
2017-07-10 09:46:16 +0800

    Initial commit

---
+++ app/src/main/java/com/bwei/gaoxiaoyan/gaoxiaoyan20170710_1504d/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bwei.gaoxiaoyan.gaoxiaoyan20170710_1504d;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+   
+
+
+    }
+}
